4175be7f2256c5353636247d6a355e26ef687406
Julian Bock <gusbocju@student.gu.se>
2017-10-07 01:04:51 +0200

    some refactoring, signing up *should* work... couldn't verfiy though

---
+++ public/javascripts/authentication.js
@@ -0,0 +1,42 @@
+/**
+ * Created by julian.bock on 2017-10-06.
+ */
+
+function signUp(email, password, passwordrepeat) {
+    if (password == passwordrepeat) {
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+        });
+    } else alert("Oi! There seems to be a mismatch with the given passwords.");
+}
+
+function signIn(email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+    });
+}
+
+function fetchUser() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            // ...
+        } else {
+            // User is signed out.
+            // ...
+        }
+    });
+}
