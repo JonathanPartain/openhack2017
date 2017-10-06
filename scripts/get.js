8cf7e44305d2becb1dc4beccf8cea25cffc4cacf
Jonathan Partain <jonathan.partain@gmail.com>
2017-10-06 22:02:45 +0200

    Added javascript folder and function for a get request

---
+++ scripts/get.js
@@ -0,0 +1,13 @@
+function getRequest(url) {
+
+    let xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            console.log(xmlHttp.responseText);
+    }
+
+    xmlHttp.open('GET', url, true); // false for synchronous request
+    xmlHttp.send( null );
+
+}
